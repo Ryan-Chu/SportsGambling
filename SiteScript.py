9b84145b37a7a86e550f5dcabe48f8f2d7ab932a
Ryan-Chu <Ryan.chu1688@gmail.com>
2023-10-28 00:22:13 -0700

    Finish list of sites and selectors for each site

--- SiteScript.py
+++ SiteScript.py
@@ -1,9 +1,37 @@
+from typing import Iterable
 import scrapy
+from scrapy.http import Request
+from items import *
 
-SitesURL = ["https://sportsbook.fanduel.com/", "https://sportsbook.draftkings.com/?category=live-in-game&subcategory=basketball", ""]
-SitesSelector = ["Fanduel Selector", "DraftKings Selector"]
+
+# 1's are placeholder for xpath or css selector for url
+FanduelSelector = Selectors(1, 1, 1, 1, 1)
+DraftKingsSelector = Selectors(1, 1, 1, 1, 1)
+
+SitesSelector = [FanduelSelector, DraftKingsSelector]
+Sitesurl = [
+            "https://sportsbook.fanduel.com/",
+            "https://sportsbook.draftkings.com/?category=live-in-game&subcategory=basketball"
+        ]
 
 class SiteScript(scrapy.Spider):
-    name = "SiteScript"
-    start_urls = SitesURL
-    
+    name = "SiteInfo"
+
+    def __init__(self, site=None, *args, **kwargs):
+        super(SiteScript, self).__init__(*args, **kwargs)
+        self.start_urls = [Sitesurl[site]]
+        self.selector = SitesSelector[site]
+
+    def parse(self, response):
+        item = GamblingItem()
+        item.Site = self.selector[1]
+        item.Sport = self.selector[2]
+        item.Odds = self.selector[3]
+        item.Subject = self.selector[4]
+        item.Bet = self.selector[5]
+        
+        
+        yield item
+
+
+#main script iterates through sites and run spider on it
---
+++ items.py
@@ -0,0 +1,22 @@
+
+import scrapy
+
+class Selectors():
+    def __init__(self, Site, Sport, Odds, Subject, Bet):
+        self.Site = Site
+        self.Sport = Sport
+        self.Odds = Odds
+        #Subject is the player or team
+        self.Subject = Subject
+        #Bet is the actual goal needed
+        self.Bet = Bet
+
+class GamblingItem(scrapy.Item):
+    Site = scrapy.Field()
+    Sport = scrapy.Field()
+    Odds = scrapy.Field()
+    #Subject is a player/team bet is on
+    Subject = scrapy.Field()
+    #Bet = what the player has to accomplish
+    Bet = scrapy.Field()
+
